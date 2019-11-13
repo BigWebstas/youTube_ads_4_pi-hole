e215d355a91e50aaa4f086669576d48c105fbf91
Joshua Webster <2896329+BigWebstas@users.noreply.github.com>
2019-11-13 13:17:37 -0700

    Update youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -1,37 +1,41 @@
 #!/bin/sh
-# Update the pihole list with youtube ads 
-# this shell script is made by Kiro 
-#Thank you for using it and enjoy 
+# Update the pihole list with youtube ads
+# this shell script is made by Kiro
+#Thank you for using it and enjoy
 
 # The script will create a file with all the youtube ads found in hostsearch and from the logs of the Pi-hole
 # it will append the list into a file called blacklist.txt'/etc/pihole/blacklist.txt'
 
-piholeIPV4=$(hostname -I |awk '{print $1}')
-piholeIPV6=$(hostname -I |awk '{print $2}')
+piholeIPV4=`hostname -i |awk '{print $1}'`
+piholeIPV6=`hostname -i |awk '{print $2}'`
 
-
-blackListFile='/etc/pihole/black.list'
+balckListFile='/etc/pihole/black.list'
 blacklist='/etc/pihole/blacklist.txt'
+# Get the domain list from hackeragent api
+# change it to be r[Number]---sn--
+# added to the youtubeFile
+sudo curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
+| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/$piholeIPV4 \1---\2-/ ">>$balckListFile
 
-# Get the list from the GitHub and add it to the blacklist.txt 
-sudo curl 'https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list'\
->>$blacklist
-sudo curl 'https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list'\
->>$blackListFile
-=======
+sudo curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
+| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/$piholeIPV6 \1---\2-/ ">>$balckListFile
 
-# Get the list from the GitHub and add it to the blacklist.txt with Pihole IPV4 and IPV6 to black.list
-sudo curl 'https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list' |awk -v a=$piholeIPV4 '{print a " " $1}'|sort |uniq>>$blackListFile
-sudo curl 'https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list' |awk -v a=$piholeIPV6 '{print a " " $1}'|sort |uniq>>$blackListFile
+sudo curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
+| awk -F, 'NR>1{print $1}'|sed "s/\(^r[[:digit:]]*\)\.\(sn\)/\1---\2-/ ">>$blacklist
 
+# collecting the youtube ads website from the pihole logs and added it the youtubeList.txt
+sudo cat /run/log/pihole/pihole*.log |grep 'r[0-9]*-.*.googlevideo'|awk -v a=$piholeIPV4 '{print a " " $8}'|sort |uniq>> $balckListFile
+sudo cat /run/log/pihole/pihole*.log |grep 'r[0-9]*-.*.googlevideo'|awk -v a=$piholeIPV6 '{print a " " $8}'|sort |uniq>> $balckListFile
+sudo cat /run/log/pihole/pihole*.log |grep 'r[0-9]*-.*.googlevideo'|awk '{print $8}'|sort |uniq>> $blacklist
 
-wait 
+wait
 
 # check to see if gawk is installed. if not it will install it
-dpkg -l | grep -qw gawk || sudo apt-get install gawk -y
+# dpkg -l | grep -qw gawk || sudo apt-get install gawk
 
-wait 
+wait
 # remove the duplicate records in place
-gawk -i inplace '!a[$0]++' $blackListFile
-wait 
+gawk -i inplace '!a[$0]++' $balckListFile
+wait
 gawk -i inplace '!a[$0]++' $blacklist
+
